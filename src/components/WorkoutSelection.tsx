2c7ded4d2ed0ca53de705ff8446d299e63e2996a
amithcabraal <147496214+amithcabraal@users.noreply.github.com>
2025-03-12 23:58:56 +0000

    Update WorkoutSelection.tsx

--- src/components/WorkoutSelection.tsx
+++ src/components/WorkoutSelection.tsx
@@ -137,7 +137,7 @@ export const WorkoutSelection: React.FC<WorkoutSelectionProps> = ({ onStartWorko
 
       {/* Right Column - Duration and Start Button */}
       <div className="flex-1 flex flex-col justify-between">
-        <div>
+        <div className="pt-12">
           <h3 className="text-lg font-semibold mb-4 flex items-center text-gray-800 dark:text-gray-200">
             <Clock className="w-5 h-5 mr-2" />
             Duration (minutes)
